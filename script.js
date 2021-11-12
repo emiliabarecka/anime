e66a458a4d0cdff7a1d1703a2ea84fa88fd01955
emiliabarecka <emiliabarecka@gmail.com>
2021-11-12 11:59:34 +0100

    add biblioteka

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const guzik = document.querySelector("button");
 const menu = document.querySelector("nav");
+// const opis = document.querySelector(".description");
 
 guzik.addEventListener("click", function(){
     console.log("click");
@@ -7,7 +8,21 @@ guzik.addEventListener("click", function(){
     guzik.classList.toggle("alfa")
 })
 
-// guzik.addEventListener("mouseover", function(){
-//     console.log("mouseover");
-//     menu.classList.toggle("d-none");
-// })
+function replaceContent(contentFromJson){
+   const content1 =  document.querySelector(".description");
+   const content2 = document.querySelector(".name");
+   const content3 = document.querySelector(".looks");
+
+   content1.textContent = contentFromJson["description"];
+   content2.textContent = contentFromJson["name"][1];  
+   content3.textContent = contentFromJson["looks"];
+}
+
+fetch("../biblioteka.json")
+    .then(response => response.json())
+    .then(data => {
+        replaceContent(data[0]);
+        // tu sprawdza czy tytuł strony jest taki jak w jsonoe
+        console.log(document.title === data[0]['title']);
+        // console.log("czy jest cielu: ", data.includes('Ciel Phantomhiwe'));   
+    });
