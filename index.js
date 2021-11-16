ec7d6e553722496dbf3639f3cf50439ca03104a8
emiliabarecka <emiliabarecka@gmail.com>
2021-11-16 10:19:13 +0100

    more classes for mainPage

--- index.js
+++ index.js
@@ -17,8 +17,15 @@ fetch("mainPage.json")
                 if(section.id == anime.id){
                     let opis = section.querySelector(".description");
                     let opis1 = section.querySelector(".description1");
+                    let info = section.querySelector(".info");
+                    let ep = section.querySelector(".epizodes");
+                    let type = section.querySelector(".type");
+
                     opis.innerHTML = anime["description"]
                     opis1.textContent = anime["description1"]
+                    info.textContent = anime["info"]
+                    ep.textContent = anime["epizodes"]
+                    type.textContent = anime["type"]
                 };
             })
            
